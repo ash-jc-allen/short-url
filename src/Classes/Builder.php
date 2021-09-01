a4dd95b543ad6908a6d22e45f901153be153a2ac
Dasari Satyam <satyam161211149@gmail.com>
2021-09-01 18:16:40 +0530

    Code style fixed

--- src/Classes/Builder.php
+++ src/Classes/Builder.php
@@ -425,7 +425,7 @@ class Builder
     {
         return ShortURL::create([
             'destination_url'                => $this->destinationUrl,
-            'default_short_url'              => route('short-url.invoke',['shortURLKey' => $this->urlKey]),
+            'default_short_url'              => route('short-url.invoke', ['shortURLKey' => $this->urlKey]),
             'url_key'                        => $this->urlKey,
             'single_use'                     => $this->singleUse,
             'track_visits'                   => $this->trackVisits,
