5d09ccdc452010e204d3c474cf26f19e7e979366
Ryan Chandler <ryangjchandler@gmail.com>
2021-10-21 17:34:11 +0100

    tests: controller & prefix

---
+++ tests/Unit/Controllers/ShortURLControllerPrefixTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace AshAllenDesign\ShortURL\Tests\Unit\Controllers;
+
+use Illuminate\Support\Facades\Config;
+use AshAllenDesign\ShortURL\Models\ShortURL;
+use AshAllenDesign\ShortURL\Tests\Unit\TestCase;
+
+class ShortURLControllerPrefixTest extends TestCase
+{
+    protected function getEnvironmentSetUp($app)
+    {
+        $app['config']->set('short-url.prefix', '/s');
+
+        parent::getEnvironmentSetUp($app);
+    }
+
+    /** @test */
+    public function visitor_is_redirected_to_the_destination_url_with_custom_prefix()
+    {
+        ShortURL::create([
+            'destination_url'      => 'https://google.com',
+            'default_short_url'    => config('app.url').'/'.config('short-url.prefix').'/12345',
+            'url_key'              => '12345',
+            'single_use'           => true,
+            'track_visits'         => true,
+            'redirect_status_code' => 301,
+            'activated_at'         => now()->subMinute(),
+        ]);
+
+        $this->get('/s/12345')->assertStatus(301)->assertRedirect('https://google.com');
+    }
+}
