c2b1d2dc27158853fbfe66fd7e93a92a00c4b17b
Victor-Emil Rossil Andersen <victor-emil@amalo.dk>
2022-03-10 21:39:29 +0100

    include slash

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,6 @@ use AshAllenDesign\ShortURL\Facades\ShortURL;
 use Illuminate\Support\Facades\Route;
 
 $prefix = ShortURL::prefix();
-$prefix = $prefix ? "/$prefix/" : '';
+$prefix = $prefix ? "/$prefix/" : '/';
 
 Route::get("$prefix{shortURLKey}", 'AshAllenDesign\ShortURL\Controllers\ShortURLController')->name('short-url.invoke');
