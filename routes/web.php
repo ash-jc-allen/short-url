088ae4f0d16daf4b9e27a82d7feba911410265b1
Victor-Emil Rossil Andersen <victor-emil@amalo.dk>
2022-03-10 20:39:09 +0100

    allow for empty short url prefix

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,7 @@
 use AshAllenDesign\ShortURL\Facades\ShortURL;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/'.ShortURL::prefix().'/{shortURLKey}', 'AshAllenDesign\ShortURL\Controllers\ShortURLController')->name('short-url.invoke');
+$prefix = ShortURL::prefix();
+$prefix = $prefix ? "/$prefix/" : '';
+
+Route::get("$prefix{shortURLKey}", 'AshAllenDesign\ShortURL\Controllers\ShortURLController')->name('short-url.invoke');
