31b54947d6fee00b1d9ad9b14fb5471f02b08351
Calvin Jackson <amigohmccclurkin98@gmail.com>
2022-10-05 02:31:12 +0300

    #161 using array keys of Agent detection rules to mimic data stored in short_url_visits table

--- src/Models/Factories/ShortURLVisitFactory.php
+++ src/Models/Factories/ShortURLVisitFactory.php
@@ -16,15 +16,17 @@ class ShortURLVisitFactory extends Factory
     {
         return [
             'ip_address' => $this->faker->ipv4(),
-            'operating_system' => $this->faker->randomElement(Agent::getPlatforms()),
+            'operating_system' => $this->faker->randomElement(
+                $this->agentArrayKeys(Agent::getPlatforms())
+            ),
             'operating_system_version' => $this->faker->randomFloat(8, 20),
             'browser' => $this->faker->randomElement(Agent::getBrowsers()),
             'browser_version' => $this->faker->userAgent(),
             'device_type' => $this->faker->randomElement(
                 array_merge(
-                    Agent::getPhoneDevices(),
-                    Agent::getTabletDevices(),
-                    Agent::getDesktopDevices(),
+                    $this->agentArrayKeys(Agent::getPhoneDevices()),
+                    $this->agentArrayKeys(Agent::getTabletDevices()),
+                    $this->agentArrayKeys(Agent::getDesktopDevices()),
                 )),
             'visited_at' => Carbon::now(),
             'referer_url' => $this->faker->url(),
@@ -33,5 +35,18 @@ class ShortURLVisitFactory extends Factory
 
             'short_url_id' => ShortURL::factory(),
         ];
+    }
+
+    /**
+     * This method grabs the keys of the properties used by Agent class as
+     * a list of detection rules. We grab the keys because they are the data
+     * being stored in this table.
+     *
+     * @param array $agentProperties
+     * @return array
+     */
+    protected function agentArrayKeys(array $agentProperties): array
+    {
+        return collect($agentProperties)->keys()->toArray();
     }
 }
