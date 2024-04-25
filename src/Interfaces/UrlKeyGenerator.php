a5aa4b7e312d5f8c5f49bf22886058c02b1e9e82
Ash Allen <mail@ashallendesign.co.uk>
2024-04-25 17:40:57 +0100

    Added a new interface for the key generator.

---
+++ src/Interfaces/UrlKeyGenerator.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AshAllenDesign\ShortURL\Interfaces;
+
+interface UrlKeyGenerator
+{
+    public function generateRandom(): string;
+
+    public function generateKeyUsing(int $seed = null): string;
+}
