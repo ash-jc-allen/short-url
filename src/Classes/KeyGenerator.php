b55e0287b291142568d48c3e047a4b86d921a73d
Steve Bauman <steven_bauman@outlook.com>
2024-07-23 11:43:43 -0400

    Increase entropy

--- src/Classes/KeyGenerator.php
+++ src/Classes/KeyGenerator.php
@@ -21,20 +21,14 @@ class KeyGenerator implements UrlKeyGenerator
     }
 
     /**
-     * Generate a unique and random URL key using the Hashids package. We start by
-     * predicting the unique ID that the ShortURL will have in the database.
-     * Then we can encode the ID to create a unique hash. On the very
-     * unlikely chance that a generated key collides with another
-     * key, we increment the ID and then attempt to create a new
-     * unique key again.
+     * Generate a unique and random URL key using the Hashids package.
+     *
+     * @return string
      */
     public function generateRandom(): string
     {
-        $ID = $this->getLastInsertedID();
-
         do {
-            $ID++;
-            $key = $this->hashids->encode($ID);
+            $key = $this->hashids->encodeHex(uniqid());
         } while (ShortURL::where('url_key', $key)->exists());
 
         return $key;
@@ -50,15 +44,5 @@ class KeyGenerator implements UrlKeyGenerator
         return $seed
             ? $this->hashids->encode($seed)
             : $this->generateRandom();
-    }
-
-    /**
-     * Get the ID of the last inserted ShortURL. This is done so that we can predict
-     * what the ID of the ShortURL that will be inserted will be called. From doing
-     * this, we can create a unique hash without a reduced chance of a collision.
-     */
-    protected function getLastInsertedID(): int
-    {
-        return ShortURL::max('id') ?? 0;
     }
 }
